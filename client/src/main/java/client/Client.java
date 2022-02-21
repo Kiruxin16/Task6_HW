825b716842f3a331965c67bbb5349282bf456f04
Kirill <kiruxin16@gmail.com>
2022-02-22 01:53:05 +0700

    Добавил клиента

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -1,4 +1,80 @@
 package client;
 
+import javafx.application.Platform;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+
+    public static ServerSocket server;
+    public static Socket socket;
+    public static final int PORT = 8189;
+    private static final String ADDRESS = "localhost";
+
+    private static DataInputStream in;
+    private static DataOutputStream out;
+
+    public static void main(String[] args) {
+
+
+        try {
+            socket=new Socket(ADDRESS, PORT);
+            in =new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(()->{
+                try{
+                    while(true){
+
+                        String  str = in.readUTF();
+                        if(str.equals("/end")){
+                            break;
+                        }
+                        System.out.println("Server: "+str);
+
+                    }
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }).start();
+
+            Scanner sc = new Scanner(System.in);
+
+            while (true){
+                String str = sc.nextLine();
+
+                if(str.equals("/end")){
+                    break;
+                }
+                out.writeUTF(str);
+            }
+
+
+
+
+
+
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,7 @@
 package server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -11,20 +13,50 @@ public class Server {
     public static Socket socket;
     public static final int PORT = 8189;
 
+    private static DataInputStream in;
+    private static DataOutputStream out;
+
     public static void main(String[] args) {
 
         try {
             Server.server = new ServerSocket(PORT);
             System.out.println("server started");
-
-
             socket = server.accept();
             System.out.println("Client connected");
 
-            Scanner sc = new Scanner(socket.getInputStream());
-            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+
+            in =new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
 
 
+
+            new Thread(()->{
+                try {
+                    while (true) {
+                       String str = in.readUTF();
+
+                        if (str.equals("/end")) {
+                            break;
+                        }
+
+                        System.out.println("Client: " + str);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }).start();
+
+
+
+            Scanner sc = new Scanner(System.in);
 
             while (true){
                 String str = sc.nextLine();
@@ -32,9 +64,12 @@ public class Server {
                 if(str.equals("/end")){
                     break;
                 }
-                System.out.println("Client: "+str);
-                out.println("ECHO: "+str);
+                out.writeUTF(str);
             }
+
+
+
+
 
         } catch (IOException e) {
             e.printStackTrace();
